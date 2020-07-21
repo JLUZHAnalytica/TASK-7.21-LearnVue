09fc3615b70529579a128d9c09352e3243ab4006
hblanx <1121688583@qq.com>
2020-07-21 17:06:36 +0800

    商家列表更新

--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,9 @@ export function login(data) {
         data: data
     })
 }
+export function shopApi(latitude,longitude){
+    return request({
+        url:"/shopping/restaurants?latitude="+latitude+"&longitude="+longitude,
+        method:"get",
+    })
+}
---
+++ src/view/shop_list/index.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+        <el-table-column
+        label="标点"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="店铺名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="店铺地址"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="description"
+        label="店铺介绍">
+      </el-table-column>
+    </el-table>
+</div>
+</template>
+<script>
+  import {shopApi} from "@/api/index"
+    export default {
+        data() {
+        return {
+          latitude:31,
+          longitude:121,
+          tableData: [{
+            name: 'name',
+            address: 'address',
+            description:"description"
+          }]
+        }
+      },
+      mounted(){
+          this.getShopList();
+      },
+        methods:{
+            handleClick(row){
+                console.log(row);
+            },
+            //deleteRow(index,row){
+            //}
+        getShopList(){
+            shopApi(this.latitude,this.longitude).then(res=>{
+                console.log(res);
+            })
+        }
+       }
+  }
+</script>
