1d7194b3fdee2b797a976a04a028dfd10476b4a8
ColdplayBaby <erhuai@github.com>
2020-07-21 19:19:23 +0800

    修改订单列表样式

--- src/view/order_list/index.vue
+++ src/view/order_list/index.vue
@@ -1,8 +1,31 @@
 <template>
-  <el-table :data="tableData" stripe style="width: 100%">
-    <el-table-column prop="id" label="订单ID" width="300"></el-table-column>
-    <el-table-column prop="total_amount" label="总价格" width="300"></el-table-column>
-    <el-table-column prop="status_bar.title" label="订单状态"></el-table-column>
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <el-form label-position="left" inline class="demo-table-expand">
+          <el-form-item label="订单ID:">
+            <span>{{ props.row.id }}</span>
+          </el-form-item>
+          <el-form-item label="订单状态:">
+            <span>{{ props.row.status_bar.title }}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="订单ID"
+      prop="id">
+    </el-table-column>
+    <el-table-column
+      label="总价格"
+      prop="total_amount">
+    </el-table-column>
+    <el-table-column
+      label="订单状态"
+      prop="status_bar.title">
+    </el-table-column>
   </el-table>
 </template>
 
