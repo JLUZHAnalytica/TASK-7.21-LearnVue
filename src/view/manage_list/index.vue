93b0e8618fcb6c4b52113892c6001b9821986039
IVE <56904267+lllw3@users.noreply.github.com>
2020-07-21 21:17:54 +0800

    管理者列表

---
+++ src/view/manage_list/index.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <el-table :data="tableData" style="width:100%">
+        <el-table-column prop="user_name" label="姓名" width=“180></el-table-column>
+        <el-table-column prop="create_time" label="注册日期" width=“180></el-table-column>
+        <el-table-column prop="city" label="地址" width=“180></el-table-column>
+        <el-table-column prop="admit" label="权限" width=“180></el-table-column>
+    </el-table>
+</div>
+</template>
+<script>
+    import {managelist} from '@/api/manage_list'//管理员列表
+    export default{
+        data(){
+            return{
+                tableData:[{
+                    user_name:'user_name',
+                    create_time:'create_time',
+                    city:'city',
+                    admit:'admit'
+                }],
+                list:{
+                    limit:20,
+                    offset:0
+                }
+            };
+        },
+        mounted(){
+            this.getList();
+        },
+        methods:{
+            handleOpen(){
+
+            },
+            handleClose(){
+
+            },
+            getList(){
+                managelist(this.list).then(res=>{
+                    this.tableData=res.data
+                    console.log(res)
+                })
+            }
+        }
+    }
+</script>
