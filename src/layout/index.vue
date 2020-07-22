3cc69fa8ea77e997b4196e99956e4815f1c97798
Weihong-Liu <63888590@qq.com>
2020-07-22 09:13:16 +0800

    Update index.vue

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <el-container style="height: 100%; border: 1px solid #eee">
-      <el-aside width="250px" style="background-color: rgb(238, 241, 246)">
+      <el-aside width="260px" style="background-color: rgb(84,92,100)">
         <el-menu
-          :default-openeds="['1', '2']"
+          :default-openeds="['1', '3']"
           default-active="2"
           class="el-menu-vertical-demo"
           @open="handleOpen"
@@ -12,13 +12,9 @@
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-menu-item index="0">
-            <i class="el-icon-menu"></i>
-            <router-link to="/manage">首页</router-link>
-          </el-menu-item>
           <el-submenu index="1">
             <template slot="title">
-              <i class="el-icon-document"></i>数据管理
+              <i class="el-icon-s-custom"></i>数据管理
             </template>
             <el-menu-item-group>
               <!-- <template slot="title">分组一</template> -->
@@ -30,9 +26,9 @@
               </el-menu-item>
             </el-menu-item-group>
             <!-- <el-menu-item-group title="分组2"> -->
-            <el-menu-item index="1-3">
-              <router-link to="/foodlist">食品列表</router-link>
-            </el-menu-item>
+              <el-menu-item index="1-3">
+                <router-link to="/foodlist">食品列表</router-link>
+              </el-menu-item>
             <!-- </el-menu-item-group> -->
             <el-menu-item index="1-4">
               <router-link to="/orderlist">订单列表</router-link>
@@ -49,10 +45,10 @@
               <!-- <template slot="title">分组一</template> -->
               <el-menu-item index="2-1">
                 <router-link to="/addshop">添加商铺</router-link>
-              </el-menu-item>
+                </el-menu-item>
               <el-menu-item index="2-2">
                 <router-link to="/addgoods">添加商品</router-link>
-              </el-menu-item>
+                </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
           <el-submenu index="3">
@@ -76,7 +72,7 @@
       </el-aside>
 
       <el-container>
-        <!-- <el-header style="text-align: right; font-size: 12px">
+        <el-header style="text-align: right; font-size: 12px">
           <el-dropdown>
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
@@ -85,7 +81,7 @@
               <el-dropdown-item>删除</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-        </el-header>-->
+        </el-header>
 
         <el-main>
           <router-view />
@@ -102,17 +98,16 @@ export default {};
 <style scoped>
 .el-submenu .el-menu-item .is-active a,
 .el-menu-item.is-active a {
-  color: #20a0ff !important;
+  color: #20a0ff!important;
 }
 .el-menu-item a {
   color: #fff;
   text-decoration: none;
 }
-.el-aside ul {
-  text-align: left;
+html,body{
+  height: 100%;
 }
-html,
-body {
-  width: 100%;
+.el-aside{
+  height: 800px;
 }
 </style>
