52f5df3c9e6ac8a5d0ba7a8e312e8b65d2c8d120
Spirit-H <844859054@qq.com>
2020-07-22 15:21:03 +0800

    test3

--- src/view/shop_list/dialog_addfood.vue
+++ src/view/shop_list/dialog_addfood.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+        <el-button type="text" @click="afdialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
 
-<el-dialog title="添加食品" :visible.sync="dialogFormVisible">
+<el-dialog title="添加食品" :visible.sync="afdialogFormVisible">
   <el-form :model="form">
-    <el-form-item label="食品名称" :label-width="formLabelWidth">
+    <el-form-item label="食品名称" :label-width="afformLabelWidth">
       <el-input v-model="form.foodname" autocomplete="off"></el-input>
     </el-form-item>
-    <el-form-item label="食品活动" :label-width="formLabelWidth">
+    <el-form-item label="食品活动" :label-width="afformLabelWidth">
       <el-input v-model="form.activities" autocomplete="off"></el-input>
     </el-form-item>
-    <el-form-item label="食品详情" :label-width="formLabelWidth">
+    <el-form-item label="食品详情" :label-width="afformLabelWidth">
       <el-input v-model="form.details" autocomplete="off"></el-input>
     </el-form-item>
-    <el-form-item label="上传食品图片" :label-width="formLabelWidth">
+    <el-form-item label="上传食品图片" :label-width="afformLabelWidth">
         <el-upload
         action="https://jsonplaceholder.typicode.com/posts/"
         list-type="picture-card"
@@ -25,22 +25,22 @@
         <img width="100%" :src="dialogImageUrl" alt="">
         </el-dialog>
     </el-form-item>
-    <el-form-item label="食品特点" :label-width="formLabelWidth">
+    <el-form-item label="食品特点" :label-width="afformLabelWidth">
       <el-select v-model="form.regionbox" placeholder="请选择" style="">
         <el-option label="区域一" value="shanghai"></el-option>
         <el-option label="区域二" value="beijing"></el-option>
       </el-select>
     </el-form-item>
-    <el-form-item label="食品规格" :label-width="formLabelWidth">
-        <el-checkbox-group v-model="checkList">
+    <el-form-item label="食品规格" :label-width="afformLabelWidth">
+        <el-checkbox-group v-model="checkList" class="standardbox">
             <el-checkbox label="单规格"></el-checkbox>
             <el-checkbox label="多规格"></el-checkbox>
         </el-checkbox-group>
     </el-form-item>
   </el-form>
   <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    <el-button @click="afdialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="afdialogFormVisible = false">确 定</el-button>
   </div>
 </el-dialog>
     </div>
@@ -53,7 +53,7 @@
           checkList: ['单规格','双规格'],
             dialogImageUrl: '',
             dialogVisible: false,
-            dialogFormVisible: false,
+            afdialogFormVisible: false,
             form: {
             foodname: '',
             activities:'',
@@ -66,7 +66,7 @@
             afresource: '',
             afdesc: ''
         },
-        formLabelWidth: '120px'
+        afformLabelWidth: '120px'
       };
     },
     methods: {
@@ -80,3 +80,14 @@
     }
   };
 </script>
+
+<style>
+    .standardbox.el-checkbox{
+        border: 1px solid black;
+        padding-left: 0px;
+    }
+    .standardbox{
+        /* border: 1px solid black; */
+        width: 200px;
+    }
+</style>
